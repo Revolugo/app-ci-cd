78c9be9f0d85d24d8bf3feabb03818247c3c4547
David Chriqui <david@Davids-MacBook-Pro-2.local>
2022-01-18 22:01:46 +0100

    WIP: ci-cd-pipeline.rb

--- ci-cd-pipeline.rb
+++ ci-cd-pipeline.rb
@@ -1,3 +1,16 @@
+require "base64"
+
+content = File.read('Input.txt')
+content.gsub!('\\r', "\r")
+content.gsub!('\\n', "\n")
+decode_base64_content = Base64.decode64(ENV["GCLOUD_API_KEYFILE"])
 puts '==================================='
-puts ENV
+puts decode_base64_content
 puts '==================================='
+File.open("~/.gcloud-api-key.json", "wb") do |f|
+  f.write(decode_base64_content)
+end
+
+IMAGE_REGISTRY_URL=$( sh ./app-ci-cd/docker.sh GET_REGISTRY_URL )
+
+puts IMAGE_REGISTRY_URL
