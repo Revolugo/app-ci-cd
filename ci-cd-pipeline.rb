14041d1efb423c906fede8848555e413f8d6160f
David Chriqui <david@Davids-MacBook-Pro-2.local>
2022-01-18 22:10:22 +0100

    WIP: ci-cd-pipeline.rb

--- ci-cd-pipeline.rb
+++ ci-cd-pipeline.rb
@@ -9,3 +9,5 @@ end
 IMAGE_REGISTRY_URL = `sh ./app-ci-cd/docker.sh GET_REGISTRY_URL`
 
 puts IMAGE_REGISTRY_URL
+
+`docker login -u _json_key --password-stdin #{IMAGE_REGISTRY_URL} < ~/.gcloud-api-key.json`
