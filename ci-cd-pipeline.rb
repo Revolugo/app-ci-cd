d98b0eaa396eb513dfa5ff5c8d77635d10d3b7e9
David Chriqui <david@Davids-MacBook-Pro-2.local>
2022-01-18 22:17:39 +0100

    WIP: ci-cd-pipeline.rb

--- ci-cd-pipeline.rb
+++ ci-cd-pipeline.rb
@@ -2,10 +2,12 @@ require "base64"
 
 GCLOUD_API_KEYFILE = Base64.decode64(ENV["GCLOUD_API_KEYFILE"])
 
-`echo #{GCLOUD_API_KEYFILE} > ~/.gcloud-api-key.json`
+File.open("/root/.gcloud-api-key.json", "w+") do |f|
+  f.write(GCLOUD_API_KEYFILE)
+end
 
 IMAGE_REGISTRY_URL = `sh ./app-ci-cd/docker.sh GET_REGISTRY_URL`
 
 puts IMAGE_REGISTRY_URL
 
-`docker login -u _json_key --password-stdin #{IMAGE_REGISTRY_URL} < ~/.gcloud-api-key.json`
+`docker login -u _json_key --password-stdin #{IMAGE_REGISTRY_URL} < /root/.gcloud-api-key.json`
