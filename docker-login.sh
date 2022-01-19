fe4a6aa117c43af95678609997f9c8091b62b24f
David Chriqui <david@Davids-MacBook-Pro-2.local>
2022-01-19 15:18:31 +0100

    WIP: ci-cd-pipeline.rb

--- docker-login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo $GCLOUD_API_KEYFILE | base64 -d >~/.gcloud-api-key.json
-
-IMAGE_REGISTRY_URL=$(sh ./app-ci-cd/docker.sh GET_REGISTRY_URL)
-
-docker login -u _json_key --password-stdin $IMAGE_REGISTRY_URL <~/.gcloud-api-key.json
